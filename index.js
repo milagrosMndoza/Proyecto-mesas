c9516aeca81d81982a3dc8f7aa668ad4f008febf
milagrosMendoza <milagrosmndoza@gmail.com>
2018-03-02 11:33:29 -0500

    empezando proyecto mesas

---
+++ index.js
@@ -0,0 +1,6 @@
+var addbutton = document.getElementById("addbutton");
+addbutton.addEventListener("click", function() {
+  var boxes = document.getElementById("boxes");
+  var clone = boxes.firstElementChild.cloneNode(true);
+  boxes.appendChild(clone);
+});
